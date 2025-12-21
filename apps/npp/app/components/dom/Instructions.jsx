2577ccea9707e2b72bc14452dc235ce51882bc02
cgkoswund <tnyaffram@gmail.com>
2025-12-21 22:03:11 +0000

    Create Instructions.jsx

---
+++ apps/npp/app/components/dom/Instructions.jsx
@@ -0,0 +1,15 @@
+const Instructions = () => {
+  return (
+    <>
+      <div>
+        <h1>Instructions</h1>
+        <p>Click and drag to rotate the camera.</p>
+        <p>Scroll to zoom in and out.</p>
+        <p>WASD to move</p>
+        <p>Space to jump</p>
+      </div>
+    </>
+  );
+};
+
+export default Instructions;
